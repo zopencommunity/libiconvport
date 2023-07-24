e994900199975c4e3405cc4119e345d2ca88be0f
Igor Todorovski <itodorov@ca.ibm.com>
2023-07-24 11:34:09 -0400

    Fix groovy scripts to add build line

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -10,6 +10,6 @@ node('linux')
                         userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libiconvport.git"]]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libiconvport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU libiconv provides an implementation of the iconv() function and the iconv program for character set conversion. ' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libiconvport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU libiconv provides an implementation of the iconv() function and the iconv program for character set conversion. ' ), string(name: 'BUILD_LINE', value: 'STABLE')]
   }
 }
