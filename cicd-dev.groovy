d2b2844daff18534b2f9190db6affc20e2834114
Igor Todorovski <itodorov@ca.ibm.com>
2023-07-24 11:23:04 -0400

    Add cicd files

---
+++ cicd-dev.groovy
@@ -0,0 +1,23 @@
+node('linux')
+{
+  stage ('Poll') {
+                // Poll from upstream:
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/master']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://git.savannah.gnu.org/git/libiconv.git"]]])
+               
+                // Poll for local changes
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libiconvport.git"]]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libiconvport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU libiconv provides an implementation of the iconv() function and the iconv program for character set conversion. ' )]
+  }
+}
---
+++ cicd-stable.groovy
@@ -0,0 +1,15 @@
+node('linux')
+{
+  stage ('Poll') {
+                // Poll for local changes
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libiconvport.git"]]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libiconvport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU libiconv provides an implementation of the iconv() function and the iconv program for character set conversion. ' )]
+  }
+}
