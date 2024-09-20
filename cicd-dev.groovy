59b9948adbd7238f5a83626bc39a6db552d731d5
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:36:19 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -15,9 +15,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libiconvport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libiconvport.git"]]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libiconvport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU libiconv provides an implementation of the iconv() function and the iconv program for character set conversion. ' ), string(name: 'BUILD_LINE', value: 'DEV')]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libiconvport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU libiconv provides an implementation of the iconv() function and the iconv program for character set conversion. ' ), string(name: 'BUILD_LINE', value: 'DEV')]
   }
 }
